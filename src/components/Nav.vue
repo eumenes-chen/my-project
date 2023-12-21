28e1490a8bbcfe72d5a01fb08e05bfa7a1dc5537
陈志岩 <zhiyanchen010964@sohu-inc.com>
2023-12-21 16:09:59 +0800

    顶部导航

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,55 +1,90 @@
 <template>
-    <div id="nav">
-      <div class="nav-list" @click="chooseNav">
-        <div class="nav-item" :routerName="item.route" v-for="(item, index) in navList" :key="index">{{ item.title }}</div>
+  <div id="nav">
+    <div class="nav-list" @click="chooseNav">
+      <div
+        :class="{ 'nav-item': true, 'is-active': item.active }"
+        :routerName="item.route"
+        v-for="(item, index) in navList"
+        :key="index"
+      >
+        {{ item.title }}
       </div>
     </div>
+  </div>
 </template>
 
 <script setup>
+import { useRouter } from "vue-router";
+import { onMounted, ref, reactive, watch } from "vue";
 
-import { RouterLink, RouterView,useRouter } from 'vue-router'
-const router = useRouter();
+const $router = useRouter();
 const props = defineProps({
-  navList:{
-    type:Array,
-    default:[]
-  }
-})
+  navList: {
+    type: Array,
+    default: [],
+  },
+});
+const navList = reactive(props.navList);
+const path = ref($router.currentRoute);
+// 监听地址路由
+watch(path, (newVal) => {
+  updateNavList();
+});
+const init = () => {
+  console.log("初始化", path.value);
+};
 const chooseNav = (e) => {
-  let routerName = e.target.getAttribute('routerName');
-  console.log('点击了nav',routerName)
-  router.push({path:routerName})
-  // VueRouter.push({path:'/weight'})
-}
-
+  let routerName = e.target.getAttribute("routerName");
+  $router.push({ path: routerName });
+};
+// 更新nav选中状态
+const updateNavList = () => {
+  navList.forEach((item) => {
+    let pathReg = new RegExp(item.route);
+    item.active = path.value.path.search(pathReg) > -1 ? true : false;
+  });
+};
+onMounted(() => {
+  init();
+});
 </script>
 
 <style scoped lang="scss">
 #nav {
-    width: 100%;
-    height: 50px;
-    position: fixed;
-    background: rgb(201, 255, 255);
-    top: 0;
-    /* background-color: orange; */
-    .nav-list {
-      height: 100%;
-      display: flex;
-      width: 1280px;
-      margin: 0 auto;
-      flex-direction: row;
-      .nav-item {
-        width: 100px;
-        text-align: center;
-        font-size: 20px;
-        line-height: 50px;
-        &:hover{
-          font-weight: 800;
-          color: yellowgreen;
-          cursor: pointer;
-        }
+  width: 100%;
+  height: 50px;
+  position: fixed;
+  background: linear-gradient(
+    to bottom,
+    rgb(156, 252, 255),
+    rgb(214, 254, 255)
+  );
+  top: 0;
+  /* background-color: orange; */
+  .nav-list {
+    height: 100%;
+    display: flex;
+    width: 1280px;
+    margin: 0 auto;
+    flex-direction: row;
+    .nav-item {
+      width: 100px;
+      text-align: center;
+      font-size: 18px;
+      line-height: 50px;
+      color: rgb(88, 88, 88);
+      letter-spacing: 3px;
+      margin-right: 14px;
+      &:hover {
+        font-weight: 800;
+        color: rgb(0, 85, 85);
+        cursor: pointer;
+      }
+      &.is-active {
+        font-weight: bold;
+        border-bottom: 3px solid aqua;
       }
     }
   }
+}
 </style>
